3eec982d906a728864ce8ce32ad317ca3f74216c
komagata-05 <35950471+komagata-05@users.noreply.github.com>
2018-03-06 12:27:13 +0900

    20180306_セッション対応
    ＜修正ファイル＞
    /export/www/star-ch-staging-test/html/mystarclub/login.php
    /export/www/star-ch-staging-test/html/internettv_alias/register/complete.php
    /export/www/star-ch-staging-test/html/mystarclub/register/complete.php
    /export/www/star-ch-staging-test/phplib/membertran.php
    
    ＜作業内容＞
    ・ITV本登録完了時にDBにsessionIDを保存、sessionIDのcookieを有効期限0で保存するよう変更
    ・MSC本登録完了時にDBにsessionIDを保存、sessionIDのcookieを有効期限0で保存するよう変更
    ・ログイン時にsessionIDのcookieを有効期限0で保存するよう変更

---
+++ html/mystarclub/register/complete.php
@@ -0,0 +1,64 @@
+<?php
+require_once('../../php_path.php');
+require_once('../../init.php');
+require_once("starchcheck.php");
+require_once("starchauth.php");
+require_once("membertran.php");
+require_once("memberdao.php");
+require_once 'starchmail.php';
+
+//request
+$request = TS_Request::getInstance();
+$form = get_request_data($request);
+$keys = preg_split("/[ ]+/", "key" );
+$form = set_default_values($form, $keys);
+
+//check
+$check = new StarchCheck();
+$template->assign("check", $check);
+
+// member
+$dao = new MemberDAO;
+$member = $dao->getMemberByOneTimeKey($form["key"]);
+if ( ! $member or $member->status != '9')
+{
+    show_error($ERROR_MESSAGES['MSC_REGISTER_INVALID_KEY']);
+    exit();
+}
+
+// サービスID重複
+if (!empty($member->service_user_id)) {
+	if ($dao->getMemberByServiceUserId($member->service_user_id))
+	{
+		show_error($ERROR_MESSAGES['MSC_REGISTER_DUPLICATION_SERVICE_ID']);
+		exit();
+	}
+}
+
+//update
+$memberTran = new MemberTran();
+$form["member_id"] = $member->member_id;
+
+// 本会員登録処理
+$ret = $memberTran->updateCompleteMember($form, 'set_env');
+if( ! $ret)
+{
+    show_error($ERROR_MESSAGES['NOT_INSERT']);
+    exit();
+}
+
+// ログイン
+//$auth = new StarchAuth();
+//$auth->doLogin($member->email1, $form["password"]);
+$_SESSION["member_id"] = $member->member_id;
+
+/*
+ * 今後の他システムとの連携用にSTARCHSESSID COOKIEをデフォルト有効期限0として保存（2018.02）
+ */
+setcookie(session_name(), session_id(), -(time() + $expire), '/');
+
+$template->assign("my", $member);
+
+$template->display(get_template_filename());
+?>
+
